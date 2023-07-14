d43d403fae219a5f42fa3e34ec25a27123dd2e2a
Ariel <ariel.fuentesgarcia@gmail.com>
2023-07-14 17:33:16 -0600

    use reducer

---
+++ use-reducer/src/App.js
@@ -0,0 +1,13 @@
+import Calendario from "./components/Calendario";
+import Compra from "./components/actividad1/Compra";
+
+function App() {
+  return (
+    <>
+      {/* <Calendario /> */}
+      <Compra />
+    </>
+  );
+}
+
+export default App;
