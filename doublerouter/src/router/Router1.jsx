d1ba76e1c334045cd24950541d62009598f3bb4b
unknown <ariel.fuentesgarcia@gmail.com>
2023-08-19 19:04:50 -0600

    rutas publicas y privadas finalizado

--- doublerouter/src/router/Router1.jsx
+++ doublerouter/src/router/Router1.jsx
@@ -2,14 +2,37 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../paginas/Login";
 import Recuperacion from "../paginas/Recuperacion";
 import Router2 from "./Router2";
+import RutasPublicas from "./RutasPublicas";
+import RutasPrivadas from "./RutasPrivadas";
 
 const Router1 = () => {
   return (
     <>
       <Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='recuperacion' element={<Recuperacion />} />
-        <Route path='/*' element={<Router2 />} />
+        <Route
+          path='login'
+          element={
+            <RutasPublicas>
+              <Login />
+            </RutasPublicas>
+          }
+        />
+        <Route
+          path='recuperacion'
+          element={
+            <RutasPublicas>
+              <Recuperacion />
+            </RutasPublicas>
+          }
+        />
+        <Route
+          path='/*'
+          element={
+            <RutasPrivadas>
+              <Router2 />
+            </RutasPrivadas>
+          }
+        />
       </Routes>
     </>
   );
